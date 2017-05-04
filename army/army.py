847d0a848b73fd7e85b7186cc0d3e5fe1c1cb735
rkta <mail@rkita.de>
2017-05-04 19:54:51 +0200

    army AC

--- army/army.py
+++ army/army.py
@@ -2,60 +2,31 @@
 
 import sys
 
-sys.stdin = open('sample.input', 'r')
-
-list = []
-sublist = []
+lst = []
+sublst = []
 
 for line in sys.stdin.readlines()[2:]:
     if line != '\n':
-        sublist.append(line.rstrip())
+        sublst.append(line.rstrip())
         continue
 
-    if sublist:
-        list.append(sublist)
-    sublist = []
+    if sublst:
+        lst.append(sublst)
+    sublst = []
 
-list.append(sublist)
+lst.append(sublst)
 
-print(len(list))
-print(list)
-print()
-
-for fight in list:
-    print(fight)
+for fight in lst:
     NG, NM = fight[0].split()
     godz = fight[1].split()
     mecha = fight[2].split()
 
+    godz = [ int(i) for i in godz]
+    mecha = [ int(i) for i in mecha]
+
     if max(godz) > max(mecha):
-        print(max(godz), max(mecha))
-        print('No chance')
         print('Godzilla')
     elif max(mecha) > max(godz):
-        print(max(godz), max(mecha))
-        print('No chance')
         print('MechaGodzilla')
-
     else:
-        while len(godz) > 0 and len(mecha) > 0:
-            if min(godz) < min(mecha):
-                print('godz lose 1')
-                godz.remove(min(godz))
-                print(godz, mecha)
-                print()
-            elif min(mecha) < min(godz):
-                print('mecha lose 1')
-                mecha.remove(min(mecha))
-                print(godz, mecha)
-                print()
-            else:
-                mecha.remove(min(mecha))
-                print('mecha lose 1 coz mecha')
-                print(godz, mecha)
-                print()
-
-        if len(godz) == 0:
-            print('MechaGodzilla')
-        else:
-            print('Godzilla')
+        print('Godzilla')
