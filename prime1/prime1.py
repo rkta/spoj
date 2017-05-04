a9e1eaccc71e400633ad68a4a9c18d34bf14f57e
rkta <mail@rkita.de>
2017-05-04 15:48:54 +0200

    prime1 AC

--- prime1/prime1.py
+++ prime1/prime1.py
@@ -3,39 +3,88 @@
 import sys
 from math import sqrt
 
-list = []
+primes = [2, 3, 5, 7]
 
-for line in sys.stdin.readlines()[1:]:
-    list.append(line.split())
+limit = 32001
+pp = primes[-1]
 
-primes = [2, 3]
+while pp < limit:
+    isPrime = True
+    pp += 2
+    sqrtpp = sqrt(pp)
+    for a in primes[1:]:
+        if a > sqrtpp:
+            break
 
-for elem in list:
-    limit = int(elem[1])
-    pp = int(elem[0])
+        if not pp % a:
+            isPrime = False
 
-    if pp <= primes[-1]:
-        pp = primes[-1]
+    if isPrime:
+        primes.append(pp)
 
-    if pp % 2 == 0:
-        pp -= 1
 
-    while pp <= limit:
-        test = True
-        pp += 2
-        sqrtpp = sqrt(pp)
-        for a in primes[1:]:
-            if a > sqrtpp:
-                break
+count = int(input())
 
-            if not pp % a:
-                test = False
+for line in sys.stdin.readlines():
 
-        if test:
-            primes.append(pp)
+    m, n = line.split()
+    m = int(m)
+    n = int(n)
 
-    for prime in primes:
-        if prime >= int(elem[0]) and prime <= limit:
-            print(prime)
 
-    print()
+    primeset = []
+
+    if n <= primes[-1]:
+        for prime in primes:
+            if prime >= m and prime <= n:
+                primeset.append(prime)
+
+    elif m > primes[-1]:
+        limit = int(sqrt(n) + 1)
+
+        if m % 2 == 0:
+            start = m + 1
+        else:
+            start = m
+
+        for pp in range(start, n + 1, 2):
+            isPrime = True
+            for prime in primes:
+                if prime > limit:
+                    break
+                if pp % prime == 0:
+                    isPrime = False
+                    break
+            if isPrime:
+                primeset.append(pp)
+
+    elif m <= primes[-1] and n >= primes[-1]:
+        for prime in primes:
+            if prime >= m and prime <= n:
+                primeset.append(prime)
+
+        limit = int(sqrt(n) + 1)
+        start = primes[-1] + 2
+
+        for pp in range(start, n + 1, 2):
+            isPrime = True
+            for prime in primes:
+                if prime > limit:
+                    break
+                if pp % prime == 0:
+                    isPrime = False
+                    break
+            if isPrime:
+                primeset.append(pp)
+    else:
+        sys.exit(1)
+
+    #print('m, n: %d %d len: %s' % (m, n, len(primeset)))
+    #continue
+    for no in primeset:
+        print(no)
+
+    count -= 1
+
+    if count > 0:
+        print()
