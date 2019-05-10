c2755e992af53f53d0bb55eaeae0b694d3f8d3fd
rkta <git@rkita.de>
2019-05-10 19:44:59 +0200

    Make cpp only

--- intest/intest.cpp
+++ intest/intest.cpp
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <vector>
 
-extern "C" int intest() {
+int main(){
     int n;
     int k;
     int count = 0;
@@ -12,24 +13,12 @@ extern "C" int intest() {
     array = (int*)malloc(n * sizeof(*array));
 
     for (int i=0; i < n; ++i) {
-        scanf("%d", &array[i]);
+        auto fp = stdin;
+        fscanf(fp, "%d", &array[i]);
         if (array[i] % k == 0) {
             count++;
         }
     }
     printf("%d\n", count);
 
-    //fp = stdin;
-    //fscanf(fp, "%d\n%d\n", &NumberOfCities, &NumberOfOldRoads);
-    //for (int i = 0; i < NumberOfOldRoads; i++)
-    //{
-    //    int cityA, cityB, length;
-    //    fscanf(fp, "%d %d %d\n", &cityA, &cityB, &length);
-    //    Roads[i] = Road(cityA, cityB, length);
-    //}
-
-}
-
-int main(){
-    intest();
 }
